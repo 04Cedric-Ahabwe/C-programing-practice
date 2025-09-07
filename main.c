d8a8740a3c44cd743e7c51480124b13ce2b92e09
abahojosh <abahojosh01@gmail.com>
2025-09-07 16:36:18 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,52 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int choice,quantity,price,total,money;
-    // Display the menu
-    printf("******MENU*******\n");
-    printf("1.Soda(UGX 1000)\n");
-    printf("2.Water(UGX 1000)\n");
-    printf("3.Juice(UGX 2500)\n");
-    printf("4.Yoghurt(UGX 3500)\n");
-    printf("5.Icecream(UGX 3000)\n");
-    printf("********************\n");
+int main(){
+    int choice,quantity,money,price,total;
 
-    // Ask the customer to make a choice
-    printf("Please select an item(1-5):");
-    scanf("%d",&choice);
+    //Display menu
+printf("******MENU******\n");
+printf("1.Crest(shs.1500)\n");
+printf("2.Mirinda(shs.1700)\n");
+printf("3.Coca(shs.1800)\n");
+printf("4.Fanta(shs.1900)\n");
+printf("5.Pepsi(shs.1300)\n");
+printf("***************\n");
 
-    // Set price based on the selection
-    switch(choice){
-case 1 :price = 1000;printf("You selected Soda.\n");break;
-case 2 :price = 1000;printf("You selected Water.\n");break;
-case 3 :price = 2500;printf("You selected Juice.\n");break;
-case 4 :price = 3500;printf("You selected Yoghurt.\n");break;
-case 5 :price = 3000;printf("You selected Icecream .\n");break;
-default:printf("Invalid choice! Please try again.\n");
-    }
-    // Ask for quantity
-    printf("Enter quantity: ");
-    scanf("%d",&quantity);
+//Ask the customer for choice
+printf("ENTER THE PRODUCT NO(1-5):  ");
+scanf("%d", &choice);
 
-    // Calculate the total price
-    total = price * quantity;
-    printf("Totalcost = UGX %d\n",total);
+// Select the price from the customers choice
+ if(choice == 1){
+        price = 1500;
+    printf("You have selected Crest.\n");
+    }else if(choice == 2){
+    price = 1700;
+    printf("You have selected Mirinda.\n");
+    }else if(choice == 3){
+    price = 1800;
+    printf("You have selected Coca.\n");
+    }else if(choice == 4){
+    price = 1900;
+    printf("You have selected Fanta.\n");
+    }else if(choice == 5){
+    price = 1300;
+    printf("You have selected Pepsi.\n");
+    }else{
+    printf("Invalid choice!!! Please try again.\n");
 
-    // Accept customer money
-    printf("Please insert money(UGX): ");
-    scanf("%d",& money);
-
-    //Check payment
-     if(money == total){
-        printf("Payment accepted.Processing transaction_ _\n");
-        } else if(money > total){
-        printf("Payment accepted.Processing transaction_ _\n");
-        printf("Your change is UGX %d.\n",money-total);
-        }else{
-        printf("Insufficient funds!!! You need UGX %d more.\n",total-money);
-        printf("Transaction failed.\n");
-        }
-         return 0;
 }
+
+// Ask the customer for quantity
+printf("ENTER THE QUANTITY OF THE PRODUCT: ");
+scanf("%d",&quantity);
+
+// Calculate the total
+total = quantity * price;
+printf("Totalcost = UGX %d\n",total);
+
+//Ask the customer for the money
+printf("Please insert money (UGX): ");
+scanf("%d",&money);
+
+// Check payment
+if(money == total){
+  printf("Payment accepted. Transaction processing.\n");
+}else if(money >  total){
+printf("Payment accepted. Transaction processing.\n");
+printf("Your change is UGX %d.\n",money-total);
+}else{
+printf("Insufficient funds!! You need UGX %d more.\n",total-money);
+printf("Transaction failed.\n");
+}
+return 0;
+}
+
